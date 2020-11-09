7870337060ffda96b21d2d66d197ec2244eae534
Louise <louise@louisefindlay.com>
2020-11-09 16:05:18 +0000

    Replaced site files with my own

---
+++ _data/sites/static.js
@@ -0,0 +1,11 @@
+module.exports = {
+    name: "Static Sites", // optional, falls back to object key
+    description: "Static sites for clients",
+    options: {
+        frequency: 60 * 23, // (in minutes), 23 hours
+    },
+    urls: [
+		"https://hairbyjuliehairdressers.co.uk",
+        "https://birkentree.com",
+	]
+};
---
+++ _data/sites/wordpress.js
@@ -0,0 +1,21 @@
+module.exports = {
+    name: "WordPress", // optional, falls back to object key
+    description: "WordPress sites I've developed for clients",
+    // skip if localhost
+    // skip if this is a new fork of the speedlify (not Zach’s)
+    skip: !process.env.CONTEXT || process.env.SITE_NAME !== "speedlify",
+    options: {
+        frequency: 60 * 23, // 24 hours
+        // Use "run" if the sites don’t share assets on the same origin
+        //           and we can reset chrome with each run instead of
+        //           each site in every run (it’s faster)
+        // Use "site" if sites are all on the same origin and share assets.
+        freshChrome: "run"
+    },
+    urls: [
+		"https://thestitchfairie.co.uk",
+		"https://camperceilidh.com",
+		"https://neosome.com",
+		"https://jomccafferty.com",
+	]
+};
