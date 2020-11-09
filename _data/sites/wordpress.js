6a57df27858e820746776cdce91e38536476c922
Louise <louise@louisefindlay.com>
2020-11-09 16:08:27 +0000

    Removed skip in site configs

--- _data/sites/wordpress.js
+++ _data/sites/wordpress.js
@@ -1,9 +1,6 @@
 module.exports = {
     name: "WordPress", // optional, falls back to object key
     description: "WordPress sites I've developed for clients",
-    // skip if localhost
-    // skip if this is a new fork of the speedlify (not Zach’s)
-    skip: !process.env.CONTEXT || process.env.SITE_NAME !== "speedlify",
     options: {
         frequency: 60 * 23, // 24 hours
         // Use "run" if the sites don’t share assets on the same origin
